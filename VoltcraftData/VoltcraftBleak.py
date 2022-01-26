70b2c7f5a7e00538bdd8a7be46e9e483fe3ffa20
BlackC0ffee <BlackC0ffee@users.noreply.github.com>
2022-01-26 22:24:29 +0100

    Fix in script and media

--- VoltcraftData/VoltcraftBleak.py
+++ VoltcraftData/VoltcraftBleak.py
@@ -8,7 +8,6 @@ BASE_UUID = "0000{}-0000-1000-8000-00805f9b34fb"
 
 MODEL_NBR_UUID = BASE_UUID.format("2a24") #reads model > https://github.com/JorenSix/TgForceSensor
 WRITE_WITHOUT_NOTIFY = BASE_UUID.format("fff3")
-READ1 = BASE_UUID.format("fff1")
 CHARACTERISTIC_UUID = BASE_UUID.format("fff4")
 
 def ReturnVoltCraftObject( str ):
@@ -16,7 +15,8 @@ def ReturnVoltCraftObject( str ):
     if len(str) == 38:
         Wattage = int(str[12:-22], 16)/1000
         Voltage = int(str[16:-20], 16)
-        Current = int(str[18:-16], 16)/100
+        Current = int(str[18:-16], 16)/1000
+        #Current = str[18:-16]
         Frequency = int(str[22:-14], 16)
         return Wattage, Voltage, Current, Frequency
     else:
@@ -38,11 +38,10 @@ async def main(address):
         #Next part just prints all characteristics (For Science)
         #for service in client.services:
             #for char in service.characteristics:
-                #print(f"Type {char.properties} - UUID {char.uuid} - Handle { char.handle}  ")
+                #print(f"Type {char.properties} - UUID {char.uuid} - Handle {char.handle} ")
 
-        await client.write_gatt_char(WRITE_WITHOUT_NOTIFY, bytes.fromhex('0f0c170000000000000000000018ffff')) #TODO - The Numbers Mason! What do they Mean?
+        #await client.write_gatt_char(WRITE_WITHOUT_NOTIFY, bytes.fromhex('0f0c170000000000000000000018ffff')) #TODO - The Numbers Mason! What do they Mean?
         await asyncio.sleep(1)
-        print(f"Read {READ1}: {await client.read_gatt_char(READ1)}") #TODO
         await client.write_gatt_char(WRITE_WITHOUT_NOTIFY, bytes.fromhex('0f0c0100152716120807e500005affff')) #Instruction Blue Blink light turns solid Green
         await asyncio.sleep(0.5)
 
